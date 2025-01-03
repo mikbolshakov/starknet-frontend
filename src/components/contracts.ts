99c62289f3a3a3e38aed42b348341d2b1ad351a6
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2025-01-03 12:55:45 +0300

    fixed low and high amount

--- src/components/contracts.ts
+++ src/components/contracts.ts
@@ -1,5 +1,5 @@
 import { ArgentTMA, type SessionAccountInterface } from "@argent/tma-wallet";
-import type { Call, Contract } from "starknet";
+import { uint256, type Call, type Contract } from "starknet";
 
 export const initWallet = (contractAddress: string) =>
   ArgentTMA.init({
@@ -17,14 +17,6 @@ export const initWallet = (contractAddress: string) =>
     },
   });
 
-function toU256(amount: number) {
-  const bigIntAmount = BigInt(amount);
-  return {
-    low: bigIntAmount,
-    high: BigInt(0),
-  };
-}
-
 export async function executeContractAction(
   contract: Contract,
   account: SessionAccountInterface,
@@ -32,7 +24,7 @@ export async function executeContractAction(
   action: string,
   amount: number
 ) {
-  const u256Amount = toU256(amount);
+  const u256Amount = uint256.bnToUint256(amount);
 
   const call: Call = {
     contractAddress: contract.address,
