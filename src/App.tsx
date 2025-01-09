615136f1803f6758512f6949e15644deebc0ea71
mikbolshakov <107908985+mikbolshakov@users.noreply.github.com>
2025-01-09 12:12:31 +0300

    fixed imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
 import { useEffect, useState } from "react";
 import { SessionAccountInterface } from "@argent/tma-wallet";
-import starknet from "starknet";
+import {
+  uint256,
+  RpcProvider,
+  Account,
+  Contract,
+  AccountInterface,
+} from "starknet";
 import { initWallet } from "./components/contracts";
 import VAULT from "./ABI/VAULT_ABI.json";
 import ERC20 from "./ABI/ERC20_ABI.json";
 import "./App.css";
 
 const APPROVE_AMOUNT = BigInt(2000000000000000000n).toString(); // 2 STRK
-const WITHDRAW_AMOUNT = starknet.uint256.bnToUint256(500000000000000000n); // 0.5 STRK
+const WITHDRAW_AMOUNT = uint256.bnToUint256(500000000000000000n); // 0.5 STRK
 const VAULT_ADDRESS =
   "0x0023611dcff5bd0be494391d54c690c413e8fce631432c21cd629bbb62641e0e";
 const STRK_ADDRESS =
   "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d";
 
-const provider = new starknet.RpcProvider({
+const provider = new RpcProvider({
   nodeUrl: import.meta.env.VITE_RPC_URL,
 });
 const privateKey = import.meta.env.VITE_ADMIN_PRIVATE_KEY;
 const accountAddress = import.meta.env.VITE_ADMIN_ADDRESS;
 
-const ADMIN_ACCOUNT = new starknet.Account(
-  provider,
-  accountAddress,
-  privateKey
-);
-const STRK_TOKEN = new starknet.Contract(ERC20.abi, STRK_ADDRESS, provider);
+const ADMIN_ACCOUNT = new Account(provider, accountAddress, privateKey);
+const STRK_TOKEN = new Contract(ERC20.abi, STRK_ADDRESS, provider);
 
 const argentTMA = initWallet(VAULT_ADDRESS);
 
 function App() {
   const [account, setAccount] = useState<SessionAccountInterface | undefined>();
-  const [vaultContract, setVaultContract] = useState<
-    starknet.Contract | undefined
-  >();
+  const [vaultContract, setVaultContract] = useState<Contract | undefined>();
   const [isLoading, setIsLoading] = useState(false);
   const [isConnected, setIsConnected] = useState(false);
 
@@ -48,10 +48,10 @@ function App() {
         if (account?.getSessionStatus() !== "VALID") return;
 
         setVaultContract(
-          new starknet.Contract(
+          new Contract(
             VAULT.abi,
             VAULT_ADDRESS,
-            account as unknown as starknet.AccountInterface
+            account as unknown as AccountInterface
           )
         );
 
